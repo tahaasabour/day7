00b71095ab5c3a79da92cea1f9fecebd87dd7a24
Taha <Taha@gmail.com>
2023-09-27 10:12:11 +0300

    ADO.NET I

---
+++ App/App/Notes.cs
@@ -0,0 +1,23 @@
+﻿
+/*
+ 
+    - Assemblies 
+    - Arrays 
+        Copy, Resizing 
+    - Collections 
+        ArrayList, Stack, ..
+    - Enumerator Pattern, foreach
+    - Indexers
+    - Object Initializers
+    - Generics 
+    - Generic COllections
+        List<>, Stack<>
+    - Delegates 
+    - Events 
+    - Observer Patten 
+    - Lambda Expression 
+    - Anonymous Methods 
+    - Active Data Objects (ADO.NET)
+
+
+ */
---
+++ App/App/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Data.Common;
+
+public class Program
+{
+    public static void Main()
+    {
+        using (SqlConnection con 
+                = new SqlConnection(ConfigurationManager.ConnectionStrings["POSBD"].ToString()))
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select top(1) Name from Customer";
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            string NumberOfCustomers = (string)cmd.ExecuteScalar();
+            Console.WriteLine(NumberOfCustomers);
+        }
+        Console.ReadKey();
+    }
+}
