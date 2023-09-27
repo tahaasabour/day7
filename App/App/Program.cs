77ea5735a04c51cf1d90f0b7fed5f11fa0bb463e
Taha <Taha@gmail.com>
2023-09-27 10:49:34 +0300

    Part II

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -8,16 +8,37 @@ public class Program
 {
     public static void Main()
     {
-        using (SqlConnection con 
+        using (SqlConnection con
                 = new SqlConnection(ConfigurationManager.ConnectionStrings["POSBD"].ToString()))
         {
             SqlCommand cmd = con.CreateCommand();
             cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "select top(1) Name from Customer";
+            cmd.CommandText = "select * from Customer; select count(ID) from Customer";
             if (con.State != ConnectionState.Open)
                 con.Open();
-            string NumberOfCustomers = (string)cmd.ExecuteScalar();
-            Console.WriteLine(NumberOfCustomers);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (!reader.HasRows)
+                Console.WriteLine("No Customers");
+            else
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"ID: {((int)reader[0]).ToString()}, " +
+                        $"  Name: {((string)reader[1]).ToString()}, " +
+                        $"Mobile: {((string)reader[2]).ToString()}");
+                }
+
+                reader.NextResult();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"Count {(int)reader[0]}");
+                }
+
+            }
+
+
         }
         Console.ReadKey();
     }
